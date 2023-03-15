8205571310757dec65afd3160c9666915dec03ef
DESKTOP-94GCHQ9\user <aaa212188@naver.com>
2023-03-15 13:28:44 +0900

    master2

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import cv2
 
 ##함수 선언부
 
+
 ##전역 변수부
 src=None #원본 이미지 
 dst1,dst2,dst3=None,None,None #영상처리 결과 
 
 ##메인 코드부
-stc=cv2.imread('c:/images/picture01.jpg')#이미지 읽기
+stc=cv2.imread("c:/images/picture01.jpg")#이미지 읽기
+dst1=grayScale(stc)
 
 
-cv2.imshow('Src',src) #화면출력
+
+cv2.imshow("Src",src) #화면출력
+cv2.imshow("Dst1",dst1)
+
 
 ##마무리
 cv2.waitKey(0)
